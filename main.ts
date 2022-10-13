84c1d212d1cd167414360ef7d257e3c63c2a130b
AnsoncL <115674720+AnsoncL@users.noreply.github.com>
2022-10-13 09:41:04 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,93 @@
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . . . #
+            . . . . .
+            # . . . #
+            . # . # .
+            `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        . . # . .
+        # # # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # . # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # # . # #
+        # . . . #
+        . . # . .
+        # . . . #
+        # # . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        # . . . #
+        . . . . .
+        `)
+})
+basic.forever(function () {
+	
+})
